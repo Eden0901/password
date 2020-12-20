8b31662bb0163143e87930bd1fb34d31c0711202
Eden Li <zhaosl0901@gmail.com>
2020-12-19 18:01:27 -0600

    change to i > 0

--- Password.py
+++ Password.py
@@ -7,7 +7,7 @@
 
 password = 'Deskjet911'
 i = 3 #剩余机会
-while True:
+while i > 0:
 	pwd = input ('请输入密码:')
 	if pwd == password:
 		print('登入成功!')
@@ -15,5 +15,4 @@ while True:
 	else:
 		i = i - 1 
 		print('密码错误！还有', i, '次机会')
-		if i == 0: 
-			break
+		
